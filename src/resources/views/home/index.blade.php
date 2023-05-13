e13b8f9a6575e8befb1fafad2e0958c903b5c3ab
Everita Vecbērza <everita.vecberza@gmail.com>
2023-05-13 13:58:29 +0300

    general laypt  updates

---
+++ src/resources/views/home/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layout')
+
+@section('content')
+
+    <h1>PD2 - {{ $title }}</h1>
+
+<header>
+    
+</header>
+
+
+<main>
+
+    </main>
+
+
+<footer>
+    <div class="container"> 
+        <div class="row">
+            <div class="col">
+            
+            </div>
+        </div>
+    </div>
+</footer>
+
+@endsection
